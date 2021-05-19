f3b98511434c170dea0871962fd3df4cdb45bd7a
Liam Plenza <liamnplenza@gmail.com>
2021-05-19 18:46:44 -0300

    Agrego funcion tiro valido

--- prueba.py
+++ prueba.py
@@ -46,9 +46,31 @@ def contenidoColumna(numeroColumna, tablero):
         columna.append(celda)
     return columna
 
+def contenidoFila(fila, tablero):
+    mostrarCF = []
+    cf = 6 - int(fila)
+    for a in range(0, 7, +1):
+        celda = tablero[cf][a]
+        mostrarCF.append(celda)
+    return mostrarCF
+
+def filaValida (fila):
+    if 0 > fila > 7:
+        return True
+    else:
+        return False
 
 secuencia = [1, 2, 3, 4, 1, 2, 3, 4]
 tablero = tableroVacio()
 tirarFichas(tablero, secuencia)
 mostrarTablero(tablero, secuencia)
-print(contenidoColumna(2, tablero))
+print(contenidoColumna(5, tablero))
+
+fila = 6
+
+if filaValida:
+    mostrarCF = contenidoFila(fila, tablero)
+    print(mostrarCF)
+else:
+    print ("La fila indicada no corresponde a una perteneciente al talero")
+
